fa39cec52d37bfb664fc81f32961901567709160
Robert Warren <grindnmosh@gmail.com>
2013-02-16 22:42:21 -0700

    added json file. added js folder.

---
+++ js/Warren_Robert_Assignment3.js
@@ -0,0 +1,3 @@
+//alert("JavaScript works!");
+
+// code
---
+++ js/json.js
@@ -0,0 +1,35 @@
+var kidsWithValuables {
+	"kids": 
+	[ 
+		{
+		"locker": ,
+		"name": "Marcus", 
+		"valuablesInLocker":
+		}
+		{
+		"locker": ,
+		"name": "Rick", 
+		"valuablesInLocker": 
+		},
+		{
+		"locker": ,
+		"name": "Chris", 
+		"valuablesInLocker": 
+		}
+		{
+		"locker": ,
+		"name": "Michael",
+		"valuablesInLocker": 
+		}
+		{
+		"locker": ,
+		"name": "Brandon",
+		"valuablesInLocker": 
+		}
+		{
+		"locker": ,
+		"name": "Luke",
+		"valuablesInLocker": 		
+		}
+	]
+};
