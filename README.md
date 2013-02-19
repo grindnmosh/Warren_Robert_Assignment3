3a26b413171372c658999b46b66e70d37d4dee7a
Robert Warren <grindnmosh@gmail.com>
2013-02-19 08:08:48 -0700

    minor update to main js file and README file with class info and correct chapter and chapter name.

---
+++ README.md
@@ -0,0 +1,8 @@
+SDI Week 3 Project
+
+Robert Warren
+02/06/2013
+Project 2
+Teen Super Sleuth
+Chapter 3 "The Suprise"
+github: https://github.com/grindnmosh/Warren_Robert_Assignment2
--- js/Warren_Robert_Assignment3.js
+++ js/Warren_Robert_Assignment3.js
@@ -3,7 +3,7 @@ Robert Warren
 02/06/2013
 Project 2
 Teen Super Sleuth
-Chapter 2 "The Plan"
+Chapter 3 "The Suprise"
 github: https://github.com/grindnmosh/Warren_Robert_Assignment2
 */
 
